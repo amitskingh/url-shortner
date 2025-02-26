65881116c26222803eaa2061351417b414626a98
amitskingh <amitsskingh@gmail.com>
2025-02-26 21:47:53 +0530

    update

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 
 import aliasRouter from "./routes/aliasRoute";
 import analyticsRouter from "./routes/analyticsRoute";
+import authenticate from "./middlewares/authenticate";
 
 const createServer = () => {
   const app = express();
@@ -15,6 +16,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/v1", aliasRouter);
-app.use("/api/v1/analytics", analyticsRouter);
+app.use("/api/v1/analytics", authenticate, analyticsRouter);
 
 export { createServer, app };
